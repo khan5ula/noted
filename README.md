a5a7af4ba13a7e8bbdcb478b19f15701351a648d
Kristian <kristian.hannula@proton.me>
2023-11-18 15:43:37 +0200

    added clear functionality

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 Entries 📝 is a small command line utility for creating and reading quick notes. I wrote Entries because I like to take notes whenever I make system configurations. If (when) something goes wrong, it's easier to rollback when I have record of what I've done.
 
-Entries takes one argument, either `write` or `read`.
+Entries takes one argument: `write`, `read` or `clear`.
 
 Create a new entry with `write`:
 
@@ -19,6 +19,14 @@ I touched a dangerous looking setting. Things could go terribly wrong.
 
 --- Sat Nov 18 14:49:12 2023 ---
 Things went better than expected.
+```
+
+Clear entries with `clear`:
+
+```zsh
+➜  ~ entries clear            
+Are you sure you want to clear all entries? [y/N] > y
+Done.
 ```
 
 Entries is written in C.
--- main.c
+++ main.c
@@ -31,17 +31,20 @@ void new_entry(FILE *fptr, char *separator);
 void get_time(char *timestamp);
 void format_header(char *header, char *separator, char *timestamp);
 void read_entries(FILE *fptr);
+void clear_entries(FILE *fptr);
+int clear_confirmation(void);
+void default_action(void);
 
 int main (int argc, char *argv[]) {
 	FILE *fptr;
 	char separator[4] = "---";
 	char operation = '.';
-	char executablePath[1024];
+	char executablePath[900];
 	char filepath[1024];
 	int iterator = 0;
 	
 	if (readlink("/proc/self/exe", executablePath, sizeof(executablePath)) == -1) {
-		printf("Error with the directory path");
+		perror("Error with the directory path");
 		return 1;
 	}
 	
@@ -62,16 +65,19 @@ int main (int argc, char *argv[]) {
 			} else if (operation == 'r') {
 				fptr = fopen(filepath, "r");
 				read_entries(fptr);
+			} else if (operation == 'w') {
+				if (clear_confirmation() == 0)  {
+					fptr = fopen(filepath, "w");
+					clear_entries(fptr);
+				}
 			} else {
-				printf("Operation not recognized.");
-				print_options();
+				default_action();
 			  return 1;
 			}
 			fclose(fptr);
 			break;
 		default:
-			printf("Operation not recognized.");
-			print_options();
+			default_action();
 			return 1;
 	}
 
@@ -79,25 +85,27 @@ int main (int argc, char *argv[]) {
 }
 
 void print_intro(void) {
-	printf("Entries - Create and write quick notes with terminal 📝\n");
-	printf("- - - - - - - - - - - - - - - - - - - - - - - - - - - -\n");
+	printf("Make quick terminal notes with Entries 📝\n");
+	printf("- - - - - - - - - - - - - - - - - - - - -\n");
 }
 
 void print_options(void) {
-	printf("\nentries write -> create a new entry\n");
-	printf("entries read -> read all entries\n");
+	printf("entries write\t\tcreate a new entry\n");
+	printf("entries read\t\tread all entries\n");
 }
 
 char check_operation_type(char *argv[]) {
 	char operation = '.';
 	if (argv[1] == NULL) {
-		printf("Error: Argument was expected\n");
+		perror("Error: Argument was expected\n");
 	}
 
 	if (strcmp(argv[1], "write") == 0) {
 		operation = 'a';
 	} else if (strcmp(argv[1], "read") == 0) {
 		operation = 'r';
+	} else if (strcmp(argv[1], "clear") == 0) {
+		operation = 'w';
 	}
 
 	return operation;
@@ -119,6 +127,7 @@ void new_entry(FILE *fptr, char *separator) {
 	format_header(header, separator, timestamp);
 
 	fprintf(fptr, "%s%s\n", header, entry);
+	printf("\nEntry saved 📝\n");
 }
 
 void get_time(char *timestamp) {
@@ -135,5 +144,31 @@ void read_entries(FILE *fptr) {
 	while (fgets(line, 512, fptr) != NULL) {
 		printf("%s", line);
 	}
+}
+
+void clear_entries(FILE *fptr) {
+	if (ftruncate(fileno(fptr), 0) == -1) {
+        perror("Error clearing entries.");
+    } else {
+		printf("Done.\n");
+	}
+}
+
+int clear_confirmation(void) {
+	char result = 'N';
+	printf("Are you sure you want to clear all entries? [y/N] > ");
+	scanf("%c", &result);
+	
+	if (result != 'y') {
+		printf("Entries were not cleared.\n");
+		return 1;
+	}
+	
+	return 0;
+}
+
+void default_action(void) {
+	printf("Invalid Entries command.\n");
+	print_options();	
 }
 
