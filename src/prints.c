417900e6f86592f64cef7bc51eb23145c2cb640a
Kristian <kristian.hannula@proton.me>
2023-11-28 13:49:28 +0200

    Forgot last COUNT from prints

--- src/prints.c
+++ src/prints.c
@@ -15,12 +15,14 @@ void print_options(void) {
   printf("\t\t\t\tCreate a new entry (n)\n");
   printf("  all");
   printf("\t\t\t\tRead all entries (a)\n");
-  printf("  last");
-  printf("\t\t\t\tRead the last entry (l)\n");
   printf("  first");
   printf("\t\t\t\tRead the first entry (f)\n");
   printf("  first COUNT");
   printf("\t\t\tRead a desired number of entries from the start (f COUNT)\n");
+  printf("  last");
+  printf("\t\t\t\tRead the last entry (l)\n");
+  printf("  last COUNT");
+  printf("\t\t\tRead a desired number of entries from the end (l COUNT)\n");
   printf("  clear");
   printf("\t\t\t\tDelete all entries (rm)\n");
 }
